d58b79051a28a504a3b7d9ee586728636e8a0c28
mateusribeirocampos <mateusrcampos@gmail.com>
2025-08-15 09:49:28 -0300

    [200~BoardException and defensive programming - Class BoardException created in the boardgame package must include a constructor that receives a parameter of type String. - The BoardException class will be used in the Board class to throw exceptions when verifying if a position exists within the board and if there is already a piece at that position. - In the Board class, the positionExists and thereIsAPiece methods were created, both receiving a position as parameter. - OOP Topics: Exceptions and Constructors (with custom error messages)

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,18 +1,21 @@
 package boardgame;
 
-import chess.ChessPiece;
-
 public class Board {
-	
+
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
-	
+
 	public Board() {
-		
+
 	}
 
 	public Board(int rows, int columns) {
+
+		// Verify if the rows and columns have more than 1.
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -22,29 +25,50 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
 	public Piece piece(int row, int column) {
+		if(!positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
-	
+
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("There is alread a piece on the position" + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
-	
+
+	// the positionExists private method verify the conditions if row and columns
+	// have piece.
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
+	// the positionExistis public boolean return the position verified in the
+	// positionExists private boolean
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+
+	// thereIsAPiece method return the position that hold a piece or empty calling
+	// the method piece(position)
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(position) != null;
+	}
 }
